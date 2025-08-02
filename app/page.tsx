710d38a96cd78664bcc68f2d22e73f67d15306e5
AleksanderOne <44018904+AleksanderOne@users.noreply.github.com>
2025-08-02 17:23:20 +0200

    feat: add stats and services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import {getTranslations} from 'next-intl/server';
-import {auth} from '@/auth';
+import LuxMediHero from "./LuxMediHero";
+import StatsAndServicesSection from "./StatsAndServicesSection";
 
-export default async function Home() {
-  const t = await getTranslations('HomePage');
-  console.log('render',(await auth()))
+export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center py-24 px-8">
-      <h1 className="text-[#333333] text-5xl">{t('title')}</h1>
-      <h2 className="text-[#333333] text-5xl">{String((await auth())?.user?.role)}</h2>
-      
-    </div>
+    <>
+      <LuxMediHero />
+      <StatsAndServicesSection />
+    </>
   );
 }
